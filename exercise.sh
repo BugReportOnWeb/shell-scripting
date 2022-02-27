f30d21415f253304c8f8836b4897f7105f4219c9
Dev <bugstatdev@protonmail.com>
2022-02-27 14:42:32 +0530

    Checking for availabiliy of a file and printing if it can be edited or not

--- exercise.sh
+++ exercise.sh
@@ -3,5 +3,18 @@
 # Author: Dev
 # Exercise script follows from here:
 
-HOST=$(hostname)
-echo "This script is running on $HOST"
+FILE=/home/dev/Development/rickroll
+if [ -e $FILE ]
+    then
+        echo "$FILE passwords are enabled"
+        if [ -x $FILE ]
+            then
+                echo "You have permissions to edit $FILE"
+            else
+                echo "You do NOT have permissions to edit $FILE"
+        fi
+    else
+        echo "$FILE does not exist"        
+fi
+
+
