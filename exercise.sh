28cd9958cb0f4e5c4e96096e9f1a810c77680318
Dev <bugstatdev@protonmail.com>
2022-02-28 23:06:37 +0530

    Renaming file based on the file extention && having an opting to go for the current date or a custom prefix for the new file name

--- exercise.sh
+++ exercise.sh
@@ -3,13 +3,54 @@
 # Author: Dev
 # Exercise script follows from here:
 
-DIR=$1
+DATE=$(date +%F)
 
-file_count() {
-    local NUMBER=$(ls -l $1 | wc -l)
-    echo $NUMBER
+# Taking user input for the prefix of the file
+prefix_fill() {
+    echo -e "Type the prefix for your file (Default YYYY-MM-DD): " | tr -d "\n"
+    read PREFIX
+
+    if [[ $PREFIX == "" ]]
+    then
+        NEWFILE=$DATE-$FILE
+        change
+    else
+        NEWFILE=$PREFIX.$EXT
+        change
+    fi
 }
 
-echo -e "$DIR: " | tr -d '\n'
-file_count $DIR
+# Output of the changed prefix and confirming the change to be made
+change() {
+    echo -e "Old file name: " | tr -d "\n"
+    echo "$FILE"
+    echo -e "New file name: " | tr -d "\n"
+    echo "$NEWFILE"
+    
+    # Change again function
+    change_again() {
+        echo -e "Are you satisfied with the change? (Y/n): " | tr -d "\n"
+        read CHANGE
+
+        if [[ $CHANGE == "Y" ]] || [[ $CHANGE == "y" ]] || [[ $CHANGE == "" ]]
+        then
+            mv $FILE "$NEWFILE"
+        elif [[ $CHANGE == "N" ]] || [[ $CHANGE == "n" ]]
+        then
+            prefix_fill
+        else
+            echo "Invalid input. Please try again"
+            change_again
+        fi 
+    }
+
+    change_again
+}
+
+for FILE in $(ls)
+do
+    EXT=${FILE: -3}
+    echo -e "$FILE is a $EXT file. " | tr -d "\n"
+    prefix_fill
+done
 
