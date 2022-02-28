78d5ecf91053025ed029d58f61ca3dde9456f9c3
Dev <bugstatdev@protonmail.com>
2022-02-28 23:32:19 +0530

    Formating changed for the if/else and echo/read blocks

--- exercise.sh
+++ exercise.sh
@@ -7,11 +7,9 @@ DATE=$(date +%F)
 
 # Taking user input for the prefix of the file
 prefix_fill() {
-    echo -e "Type the prefix for your file (Default YYYY-MM-DD): " | tr -d "\n"
-    read PREFIX
+    echo -e "Type the prefix for your file (Default YYYY-MM-DD): " | tr -d "\n"; read PREFIX
 
-    if [[ $PREFIX == "" ]]
-    then
+    if [[ $PREFIX == "" ]]; then
         NEWFILE=$DATE-$FILE
         change
     else
@@ -22,21 +20,16 @@ prefix_fill() {
 
 # Output of the changed prefix and confirming the change to be made
 change() {
-    echo -e "Old file name: " | tr -d "\n"
-    echo "$FILE"
-    echo -e "New file name: " | tr -d "\n"
-    echo "$NEWFILE"
+    echo -e "Old file name: " | tr -d "\n"; echo "$FILE"
+    echo -e "New file name: " | tr -d "\n"; echo "$NEWFILE"
     
     # Change again function
     change_again() {
-        echo -e "Are you satisfied with the change? (Y/n): " | tr -d "\n"
-        read CHANGE
+        echo -e "Are you satisfied with the change? (Y/n): " | tr -d "\n"; read CHANGE
 
-        if [[ $CHANGE == "Y" ]] || [[ $CHANGE == "y" ]] || [[ $CHANGE == "" ]]
-        then
+        if [[ $CHANGE == "Y" ]] || [[ $CHANGE == "y" ]] || [[ $CHANGE == "" ]]; then
             mv $FILE "$NEWFILE"
-        elif [[ $CHANGE == "N" ]] || [[ $CHANGE == "n" ]]
-        then
+        elif [[ $CHANGE == "N" ]] || [[ $CHANGE == "n" ]]; then
             prefix_fill
         else
             echo "Invalid input. Please try again"
@@ -50,7 +43,6 @@ change() {
 for FILE in $(ls)
 do
     EXT=${FILE: -3}
-    echo -e "$FILE is a $EXT file. " | tr -d "\n"
-    prefix_fill
+    echo -e "$FILE is a $EXT file. " | tr -d "\n"; prefix_fill
 done
 
