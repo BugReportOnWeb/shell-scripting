76720e9e34db02b9b42f3b6c3cc2a76e8471e6e5
Dev <bugstatdev@protonmail.com>
2022-03-10 19:38:30 +0530

    Separate lines for the two comands to run

--- finish.sh
+++ finish.sh
@@ -2,4 +2,5 @@
 
 # Author: Dev
 
-set PID (ps aux | grep -i $argv | head -n 1 | perl -pe 's/^\S+\s+//g' | cut -d' ' -f1); kill $PID
+set PID (ps aux | grep -i $argv | head -n 1 | perl -pe 's/^\S+\s+//g' | cut -d' ' -f1)
+kill $PID
