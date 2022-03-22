f4304a3d157d97e8d81a7d80a802a8b4ee636ee0
Dev <flashytesting@gmail.com>
2022-03-22 21:23:15 +0530

    Add script to ease on assembly building process

---
+++ liners/assembly_comp.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+set NAME (echo $argv | cut -d. -f1)
+set EXT (echo $argv | cut -d. -f2)
+set OUTPUT "$NAME.o"
+
+function main
+    if test $EXT = "asm"
+        nasm -f elf32 $argv -o $OUTPUT && ld -m elf_i386 $OUTPUT -o ../main/$NAME && ../main/$NAME || echo "Error occured!"
+    else
+        echo "[*.asm] => acceptable argument"
+        return 1
+    end
+end
+
+main $argv
