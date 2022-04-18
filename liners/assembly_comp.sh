7f39dda5cc204ff1f43de2d317fec9d42eb18928
Dev <flashytesting@gmail.com>
2022-04-18 10:22:39 +0530

    Rename object file variable

--- liners/assembly_comp.sh
+++ liners/assembly_comp.sh
@@ -2,11 +2,11 @@
 
 set NAME (echo $argv | cut -d. -f1)
 set EXT (echo $argv | cut -d. -f2)
-set OUTPUT "$NAME.o"
+set OBJECT "$NAME.o"
 
 function main
     if test $EXT = "asm"
-        nasm -f elf32 $argv -o $OUTPUT && ld -m elf_i386 $OUTPUT -o ../main/$NAME && ../main/$NAME || echo "Error occured!"
+        nasm -f elf32 $argv -o $OBJECT && ld -m elf_i386 $OBJECT -o ../main/$NAME && ../main/$NAME || echo "Error occured!"
     else
         echo "[*.asm] => acceptable argument"
         return 1
