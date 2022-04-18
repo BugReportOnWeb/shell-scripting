80dfd258a4a151936a8e647de195c7ae4a3fd32b
Dev <flashytesting@gmail.com>
2022-04-18 10:54:46 +0530

    Add separate EXT dependent dir for file management

--- liners/comp_check.sh
+++ liners/comp_check.sh
@@ -5,10 +5,13 @@ set EXT (echo $argv | cut -d. -f2)
 function main
     switch $EXT
         case c
+            mkdir c_dir && mv $argv c_dir && cd c_dir
             gcc $argv && ./a.out
         case cpp
+            mkdir cpp_dir && mv $argv cpp_dir && cd cpp_dir
             g++ $argv && ./a.out
         case asm
+            mkdir asm_dir && mv $argv asm_dir && cd asm_dir
             nasm -f elf32 $argv && ld -m elf_i386 (echo $argv | cut -d. -f1).o && ./a.out
         case '*'
             return 1
