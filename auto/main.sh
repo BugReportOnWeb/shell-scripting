69449f8670412cdc2fd604af2916aca3efdc01ca
Dev <flashytesting@gmail.com>
2022-05-17 11:20:38 +0530

    Fix indentation/formating bug && change sleep time for negative interval from 10 to 30

--- auto/main.sh
+++ auto/main.sh
@@ -14,77 +14,77 @@ function lets_go
         # Cases for lectures on specific weekdays.
         switch $argv
             case "Monday"
-                if test 
-                    \($hour = "09" -a $min = "55" -a $value = "AM"\) -o
-                    \($hour = "12" -a $min = "40" -a $value = "PM"\) -o
-                    \($hour = "02" -a $min = "30" -a $vlaue = "PM"\)
+                if test \ 
+                    $hour = "09" -a $min = "55" -a $value = "AM" -o \
+                    $hour = "12" -a $min = "40" -a $value = "PM" -o \
+                    $hour = "02" -a $min = "30" -a $vlaue = "PM"
 
                     echo -e "Opening MAIN link..."
                     open MAIN_LINK
                     sleep 3530
-                else if test 
-                    \($hour = "09" -a $min = "00" -a $value = "AM"\)
+                else if test \ 
+                    $hour = "09" -a $min = "00" -a $value = "AM"
 
                     echo -e "Opening OTHER link"
                     open DET_LINK
                     sleep 3530
                 end
             case "Tuesday"
-                if test
-                    \($hour = "09" -a $min = "00" -a $value = "AM"\) -o
-                    \($hour = "01" -a $min = "35" -a $value = "PM"\)
+                if test \
+                    $hour = "09" -a $min = "00" -a $value = "AM" -o \
+                    $hour = "01" -a $min = "35" -a $value = "PM"
 
                     echo -e "Opening MAIN link..."
                     open MAIN_LINK
                     sleep 3530
-                else if test
-                    \($hour = "10" -a $min = "50" -a $value = "AM"\) -o
-                    \($hour = "03" -a $min = "25" -a $value = "PM"\)
+                else if test \
+                    $hour = "10" -a $min = "50" -a $value = "AM" -o \
+                    $hour = "03" -a $min = "25" -a $value = "PM"
 
                     echo -e "Opening OTHER link"
                     open DET_LINK
                     sleep 3530
                 end
             case "Wednesday"
-                if test
-                    \($hour = "09" -a $min = "00" -a $value = "AM"\) -o
-                    \($hour = "09" -a $min = "55" -a $value = "AM"\) -o
-                    \($hour = "11" -a $min = "45" -a $value = "AM"\) -o
-                    \($hour = "03" -a $min = "25" -a $value = "PM"\)
+                if test \
+                    $hour = "09" -a $min = "00" -a $value = "AM" -o \
+                    $hour = "09" -a $min = "55" -a $value = "AM" -o \
+                    $hour = "11" -a $min = "45" -a $value = "AM" -o \
+                    $hour = "03" -a $min = "25" -a $value = "PM"
 
                     echo -e "Opening MAIN link..."
                     open MAIN_LINK
                     sleep 3530
                 end
             case "Thursday"
-                if test
-                    \($hour = "09" -a $min = "00" -a $value = "AM"\) -o
-                    \($hour = "10" -a $min = "50" -a $value = "AM"\) -o
-                    \($hour = "11" -a $min = "45" -a $value = "AM"\) -o
-                    \($hour = "02" -a $min = "30" -a $value = "PM"\)
+                if test \
+                    $hour = "09" -a $min = "00" -a $value = "AM" -o \
+                    $hour = "10" -a $min = "50" -a $value = "AM" -o \
+                    $hour = "11" -a $min = "45" -a $value = "AM" -o \
+                    $hour = "02" -a $min = "30" -a $value = "PM"
 
                     echo -e "Opening MAIN link..."
                     open MAIN_LINK
                     sleep 3530
-                else if test
-                    \($hour = "01" -a $min = "35" -a $value = "PM"\)
+                else if test \
+                    $hour = "01" -a $min = "35" -a $value = "PM"
 
                     echo -e "Opening OTHER link"
                     open DET_LINK
                     sleep 3530
                 end
             case "Friday"
-                if test
-                    \($hour = "10" -a $min = "50" -a $value = "AM"\) -o
-                    \($hour = "12" -a $min = "40" -a $value = "PM"\) -o
-                    \($hour = "02" -a $min = "30" -a $value = "PM"\) -o
-                    \($hour = "03" -a $min = "25" -a $value = "PM"\)
+                if test \
+                    $hour = "10" -a $min = "50" -a $value = "AM" -o \
+                    $hour = "12" -a $min = "40" -a $value = "PM" -o \
+                    $hour = "02" -a $min = "30" -a $value = "PM" -o \
+                    $hour = "03" -a $min = "25" -a $value = "PM"
 
                     echo -e "Opening MAIN link..."
                     open MAIN_LINK
                     sleep 3530
-                else if test
-                    \($hour = "01" -a $min = "35" -a $value = "PM"\)
+                else if test \
+                    $hour = "01" -a $min = "35" -a $value = "PM"
 
                     echo -e "Opening OTHER link"
                     open DET_LINK
@@ -95,7 +95,7 @@ function lets_go
                 return 0
         end
 
-        sleep 10
+        sleep 30
     end
 end
 
