e1ef0c2bef4e02405982dd174108d2ca835c45a1
Dev <flashytesting@gmail.com>
2023-03-22 14:10:19 +0530

    Remove custom .gitignore file

--- extras/clear_react.sh
+++ extras/clear_react.sh
@@ -39,33 +39,8 @@ body {
 }
 EOF
 
-# Initilize a git repo && Add .gitignore
+# Initializing git repo && commiting the changes 
 git init
-cat <<EOF > ./.gitignore
-# dependencies
-/node_modules
-/.pnp
-.pnp.js
-
-# testing
-/coverage
-
-# production
-/build
-
-# misc
-.DS_Store
-.env.local
-.env.development.local
-.env.test.local
-.env.production.local
-
-npm-debug.log*
-yarn-debug.log*
-yarn-error.log*
-EOF
-
-# Commiting the changes
 git add .
 git commit -m "Initial commit"
 
